746caad0bd8a797936da528697d6dd220f6a0ef8
BBBys <b.borys@mailbox.org>
2026-05-20 18:00:20 +0200

    Steuerung zugefügt
    Raspberry-Touchscreen-Programm zur Steuerung

---
+++ 50Bahnsteuerung/Bahnsteuerung/fBSMain.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Borys.Bahn.Bahnsteuerung
+{
+    partial class fBSMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fBSMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.cbPort = new System.Windows.Forms.ToolStripComboBox();
+            this.bStart = new System.Windows.Forms.ToolStripButton();
+            this.bEnde = new System.Windows.Forms.ToolStripButton();
+            this.bSenden = new System.Windows.Forms.ToolStripButton();
+            this.sPort = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssl1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslVersion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lPch = new System.Windows.Forms.Label();
+            this.lDaten = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flpCmd = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpLoks = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cbPort,
+            this.bStart,
+            this.bEnde,
+            this.bSenden});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(677, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // cbPort
+            // 
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(121, 25);
+            this.cbPort.SelectedIndexChanged += new System.EventHandler(this.cbPort_SelectedIndexChanged);
+            // 
+            // bStart
+            // 
+            this.bStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bStart.Image = ((System.Drawing.Image)(resources.GetObject("bStart.Image")));
+            this.bStart.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(35, 22);
+            this.bStart.Text = "Start";
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // bEnde
+            // 
+            this.bEnde.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bEnde.Image = ((System.Drawing.Image)(resources.GetObject("bEnde.Image")));
+            this.bEnde.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bEnde.Name = "bEnde";
+            this.bEnde.Size = new System.Drawing.Size(37, 22);
+            this.bEnde.Text = "Ende";
+            this.bEnde.Click += new System.EventHandler(this.bEnde_Click);
+            // 
+            // bSenden
+            // 
+            this.bSenden.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.bSenden.Image = ((System.Drawing.Image)(resources.GetObject("bSenden.Image")));
+            this.bSenden.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bSenden.Name = "bSenden";
+            this.bSenden.Size = new System.Drawing.Size(49, 22);
+            this.bSenden.Text = "senden";
+            this.bSenden.Click += new System.EventHandler(this.bSenden_Click);
+            // 
+            // sPort
+            // 
+            this.sPort.PortName = "COM21";
+            this.sPort.ErrorReceived += new System.IO.Ports.SerialErrorReceivedEventHandler(this.sPort_ErrorReceived);
+            this.sPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sPort_DataReceived);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssl1,
+            this.tsslVersion});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 384);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(677, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssl1
+            // 
+            this.tssl1.Name = "tssl1";
+            this.tssl1.Size = new System.Drawing.Size(12, 17);
+            this.tssl1.Text = "?";
+            // 
+            // tsslVersion
+            // 
+            this.tsslVersion.Name = "tsslVersion";
+            this.tsslVersion.Size = new System.Drawing.Size(44, 17);
+            this.tsslVersion.Text = "V0.0.00";
+            // 
+            // lPch
+            // 
+            this.lPch.AutoSize = true;
+            this.lPch.Location = new System.Drawing.Point(12, 37);
+            this.lPch.Name = "lPch";
+            this.lPch.Size = new System.Drawing.Size(28, 13);
+            this.lPch.TabIndex = 2;
+            this.lPch.Text = "lPch";
+            // 
+            // lDaten
+            // 
+            this.lDaten.AutoSize = true;
+            this.lDaten.Location = new System.Drawing.Point(12, 60);
+            this.lDaten.Name = "lDaten";
+            this.lDaten.Size = new System.Drawing.Size(38, 13);
+            this.lDaten.TabIndex = 3;
+            this.lDaten.Text = "lDaten";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.flpCmd);
+            this.groupBox1.Controls.Add(this.flpLoks);
+            this.groupBox1.Location = new System.Drawing.Point(298, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 284);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bedienung";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flpCmd
+            // 
+            this.flpCmd.Location = new System.Drawing.Point(97, 19);
+            this.flpCmd.Name = "flpCmd";
+            this.flpCmd.Size = new System.Drawing.Size(247, 230);
+            this.flpCmd.TabIndex = 1;
+            // 
+            // flpLoks
+            // 
+            this.flpLoks.Location = new System.Drawing.Point(6, 19);
+            this.flpLoks.Name = "flpLoks";
+            this.flpLoks.Size = new System.Drawing.Size(85, 259);
+            this.flpLoks.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Borys.Bahn.Bahnsteuerung.Properties.Resources.maerklin_schatten;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Location = new System.Drawing.Point(0, 331);
+            this.panel1.MinimumSize = new System.Drawing.Size(146, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(146, 52);
+            this.panel1.TabIndex = 5;
+            // 
+            // fBSMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 406);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lDaten);
+            this.Controls.Add(this.lPch);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "fBSMain";
+            this.Text = "Start";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox cbPort;
+        private System.IO.Ports.SerialPort sPort;
+        private System.Windows.Forms.ToolStripButton bStart;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssl1;
+        private System.Windows.Forms.ToolStripButton bEnde;
+        private System.Windows.Forms.ToolStripButton bSenden;
+        private System.Windows.Forms.Label lPch;
+        private System.Windows.Forms.Label lDaten;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FlowLayoutPanel flpLoks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flpCmd;
+        private System.Windows.Forms.ToolStripStatusLabel tsslVersion;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ 50Bahnsteuerung/Bahnsteuerung/fBSMain.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Borys.Bahn.Bahnsteuerung
+{
+
+    /// <summary>
+    /// Text im Label aus anderem Thread setzen
+    /// </summary>
+    /// <remarks>
+    /// InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.
+    /// If these threads are different, it returns true.
+    /// </remarks>
+    public partial class fBSMain : Form
+    {
+        /// <summary>
+        /// Status des Programms
+        /// </summary>
+        enum eStatus
+        {
+            /// <summary>
+            /// Anfangsstatus
+            /// </summary>
+            nichts = 0,
+            /// <summary>
+            /// Kommunikation aufbauen
+            /// </summary>
+            Start,
+            /// <summary>
+            /// Kommunikation vorhanden
+            /// </summary>
+            Go,
+            /// <summary>
+            /// Programm wird beendet
+            /// </summary>
+            Ende
+        }
+        /// <summary>
+        /// globaler Programmstatus
+        /// </summary>
+        eStatus Status = eStatus.nichts;
+        /// <summary>
+        /// empfangenen Text weiterverarbeiten
+        /// This delegate enables asynchronous calls 
+        /// </summary>
+        /// <param name="text">der empfangene Text</param>
+        delegate void SetTextCallback(string text);
+        /// <summary>
+        /// Adresse für nächsten Befehl
+        /// </summary>
+        private uint Adresse;
+        /// <summary>
+        /// Port
+        /// </summary>
+        private string Comport;
+        /// <summary>
+        /// Baudrate, bei USB wohl unnötig
+        /// </summary>
+        private int Comrate;
+        private Properties.Settings Props;
+        private cCmd.StartbitUndBefehl Befehl;
+        private cLoks Lokliste;
+        private cCmds CmdListe;
+        private const string CmdQuit = "!!q!!";
+        private byte iTestadresse=28;
+        /// <summary>
+        /// Hauptformular Konstruktor
+        /// </summary>
+        public fBSMain()
+        {
+            InitializeComponent();
+            Text = Properties.Resources.Fenstertitel + " V" + Application.ProductVersion + ". ";
+#if DEBUG
+            Text += "DEBUGVERSION";
+#else
+            Text += Application.CompanyName + ": " + Application.ProductName;
+#endif
+            #region Einstellungen
+            Props = Properties.Settings.Default;
+            Comport = Props.Comport;
+            Comrate = Props.Comrate;
+
+            cbPort.BeginUpdate();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                cbPort.Items.Add(s);
+            }
+            cbPort.EndUpdate();
+            cbPort.Text = Comport;
+            #endregion
+            #region Objekte
+            LokButtons(flpLoks);
+            CmdButtons(flpCmd);
+            #endregion
+        }
+
+        private void CmdButtons(Panel pPanel)
+        {
+            //       Lok1 = new cLok("Lok1", 0x18, pPanel, LokAuswahl);
+            CmdListe = new cCmds(pPanel, CmdAuswahl);
+            CmdListe.Neu("STOP", 1, 0x0d);
+            CmdListe.Neu("Licht", 0, 0x10);
+            CmdListe.Neu("+", 1, 0x10);
+            CmdListe.Neu("-", 1, 0x11);
+            CmdListe.Neu("Blinklicht", 0, 0x11);
+            CmdListe.Neu("F2", 0, 0x12);
+            CmdListe.Neu("F3", 0, 0x13);
+            CmdListe.Neu("F4", 0, 0x14);
+            CmdListe.Neu("0-00", 0, 0x00);
+            CmdListe.Neu("0-01", 0, 0x01);
+            CmdListe.Neu("0-3F", 0, 0x3F);
+            CmdListe.Neu("1-3F", 0, 0x3F);
+            CmdListe.Neu("gn", 0, 0x20);
+            CmdListe.Neu("rt", 0, 0x00);
+        }
+
+        /// <summary>
+        /// Erstellen der Buttons
+        /// </summary>
+        private void LokButtons(Panel pPanel)
+        {
+            Lokliste = new cLoks(pPanel, LokAuswahl);
+            Lokliste.Neu("Lok1", 0x18);
+            Lokliste.Neu("Werkstattlok", 0x19);
+            Lokliste.Neu("Lok3", 0x1b);
+            Lokliste.Neu("Lok4", 0x1c);
+            Lokliste.Neu("0B", 0x0B);
+            Lokliste.Neu("0C", 0x0C);
+            Lokliste.Neu("0D", 0x0D);
+            Lokliste.Neu("0A", 0x0A);
+            Lokliste.Neu("09", 0x09);
+        }
+
+        /// <summary>
+        /// Callback: Lok-Button
+        /// </summary>
+        void LokAuswahl(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            LokButtonsGrau(b);
+            b.BackColor = Color.Yellow;
+            Adresse = ((cLok)b.Tag).Adresse;
+        }
+        void CmdAuswahl(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            LokButtonsGrau(b);
+            b.BackColor = Color.Yellow;
+            Befehl = ((cCmd)b.Tag).Befehl;
+        }
+
+        /// <summary>
+        /// Text aus anderm Thread setzen
+        /// </summary>
+        /// <remarks>
+        /// InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.
+        /// If these threads are different, it returns true.
+        /// </remarks>
+        private void SetText(String sEin)
+        {
+
+            if (this.lDaten.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { sEin });
+            }
+            else
+            {
+                this.lDaten.Text = sEin;
+                if (Status == eStatus.Go)
+                {
+
+                }
+                else if (Status == eStatus.Start)
+                {
+                    if (sEin.Contains("IRIF"))
+                    {
+                        tsslVersion.Text = sEin;
+                        sPort.WriteLine("go!");
+                        Status = eStatus.Go;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Verbindung herstellen
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            Status = eStatus.Start;
+            sPort.PortName = Comport;
+            sPort.BaudRate = Comrate;
+            tssl1.Text = Comport + " öffnen...";
+            try
+            {
+                sPort.Open();
+                tssl1.Text = "geöffnet";
+                //timer.Enabled = true;
+            }
+            catch (TimeoutException)
+            {
+                //timer.Enabled = false;
+                tssl1.Text = "Timeout!";
+                Status = eStatus.Ende;
+            }
+            catch (Exception ex)
+            {
+                //timer.Enabled = false;
+                tssl1.Text = ex.Message;
+                Status = eStatus.Ende;
+            }
+        }
+
+        /// <summary>
+        /// Callback: Daten angekommen
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        private void sPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String zeile = "";
+            tssl1.Text = "erwarte Daten...";
+            //int ib;
+            //ib = SP1.BytesToRead;
+            if (Status == eStatus.Ende) sPort.Close();
+            else try
+                {
+                    zeile = sPort.ReadExisting();
+                    tssl1.Text = "Daten OK: " + zeile.Length.ToString();
+                }
+                catch (TimeoutException) { tssl1.Text = "Timeout!"; Status = eStatus.Ende; }
+                catch (Exception ex) { tssl1.Text = ex.Message; Status = eStatus.Ende; }
+            SetText(zeile);
+        }
+
+        /// <summary>
+        /// beenden
+        /// </summary>
+        private void bEnde_Click(object sender, EventArgs e)
+        {
+            if (sPort.IsOpen)
+            {
+                sPort.Write(CmdQuit);
+                Status = eStatus.Ende;
+                sPort.Close(); 
+            }
+            Close();
+        }
+
+        /// <summary>
+        /// Callback: Kommunikationsfehler
+        /// </summary>
+        private void sPort_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            MessageBox.Show(e.EventType.ToString());
+        }
+
+        /// <summary>
+        /// Test: senden
+        /// </summary>
+        private void bSenden_Click(object sender, EventArgs e)
+        {
+            Byte[] senden = new Byte[5];
+            senden[0] = (Byte)'c';
+            senden[1] = (Byte)Befehl.SB2;
+            senden[2] = (Byte)Adresse;
+            senden[3] = (Byte)Befehl.B;
+            senden[4] = (Byte)'!';
+            sPort.Write(senden, 0, 5);
+        }
+
+        /// <summary>
+        /// Port ausgewählt
+        /// </summary>
+        private void cbPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ToolStripComboBox cb = (ToolStripComboBox)sender;
+            Comport = cb.Text;
+            Props.Comport = Comport;
+        }
+
+
+
+        /// <summary>
+        /// nicht gewählte Buttons Farbe ändern
+        /// </summary>
+        private static void LokButtonsGrau(Button pButton)
+        {
+            Control panel = pButton.Parent;
+            foreach (Button b in panel.Controls)
+                b.BackColor = Color.White;
+        }
+
+        /// <summary>
+        /// Test
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lPch.Text = iTestadresse.ToString();
+            try
+            {
+                Byte[] senden = new Byte[5];
+                senden[0] = (Byte)'c';
+                senden[1] = (Byte)Befehl.SB2;
+                senden[2] = (Byte)iTestadresse++;
+                senden[3] = (Byte)Befehl.B;
+                senden[4] = (Byte)'!';
+                sPort.Write(senden, 0, 5);
+                if (sPort.BytesToWrite > 1)
+                    lDaten.Text = "???";
+            }
+            catch (Exception exe) { tssl1.Text = exe.Message; }
+        }
+    }
+}
