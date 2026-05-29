9c78ac0a5321802fcb032d51698cd664560c44e1
BBBys <b.borys@mailbox.org>
2026-05-29 15:22:23 +0200

    Teilung alt/neu

--- 50Bahnsteuerung/ALTBahn/PGBahn/PFirmata/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Stil", "IDE1006:Benennungsstile", Justification = "<Ausstehend>", Scope = "member", Target = "~M:PFirmata.Form1.button2_Click(System.Object,System.EventArgs)")]
-
